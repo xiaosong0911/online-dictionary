7a2c9ccc7acd42fdccfcfb4e8d4f131b1ded2558
Xiaosong Chen <xiaosong0911@gmail.com>
2017-09-25 19:46:39 +0800

    interaction with clipboard

--- query.py
+++ query.py
@@ -7,12 +7,16 @@ def getEntry(word):
     tree = ET.ElementTree(ET.fromstring(content))
 
     ret = ''
+    for node in tree.findall('.//return-phrase'):
+        if node.text:
+            ret += node.text + '\n'
     for node in tree.findall('.//phonetic-symbol'):
         if node.text:
             ret += '/' + node.text + '/' + '\n'
     for node in tree.findall('.//translation/content'):
         if node.text:
             ret += '\t'+ node.text + '\n'
+    ret += '\n'
     ret += '[from internet]' + '\n'
     for node in tree.findall('.//yodao-web-dict/web-translation'):
         ret += '-' + node.find('key').text + '\n'
--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 import os
+import re
 import tkinter as tk
 from tkinter import scrolledtext
 
@@ -15,7 +16,7 @@ y = (win.winfo_screenheight() // 2) - (height // 2)
 win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 #win.resizable(False, False)
 
-def showEntry(event):
+def showPhrase(event):
     key = word.get()
     with open(os.path.expanduser('~/Documents/vocabulary.txt'),'a') as fo:
         fo.write(key + '\n')
@@ -24,7 +25,18 @@ def showEntry(event):
     tb.delete(1.0, tk.END)
     tb.insert(tk.INSERT, text)
     tb.config(state='disabled')
-win.bind('<Return>', showEntry)
+    win.clipboard_clear()
+    win.clipboard_append(text)
+win.bind('<Return>', showPhrase)
+
+re_phrase = re.compile('^[A-Za-z. ]*$')
+def checkClipboard(event):
+    if event.widget == ent:
+        clipboard_text = win.clipboard_get()
+        if re_phrase.match(clipboard_text):
+            ent.delete(0, 'end')
+            ent.insert('end', clipboard_text)
+win.bind('<FocusIn>', checkClipboard)
 
 word = tk.StringVar()
 ent = tk.Entry(win, text='welcome!', textvariable=word, font=font)
